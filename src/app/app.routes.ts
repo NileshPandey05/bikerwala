52196e62dfd905758b826ad67e6c607c961f87d6
Shahnawaz4518 <shahnawaz6499@gmail.com>
2025-12-15 15:24:30 +0530

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ExplorerComponent } from './components/explorer/explorer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    data: { title: 'Home' }
+  },
+   {
+    path: 'bike-brand',
+    component: BikeBrandListComponent,
+    data: { title: 'Bike Brands' }
+  },
+
+  // 🔐 Protected Bike Brand Details
+  {
+    path: 'bike-brand/:bikename',
+    component: BikeBrandDetailsComponent,
+    canActivate: [authGuard],
+    data: { title: 'Bike Brand Details' }
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+
+];
