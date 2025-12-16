1ff70ff0896034538220c44b12d96f7085551037
Nilesh Pandey <99646982+NileshPandey05@users.noreply.github.com>
2025-12-16 10:52:49 +0000

    fully updated branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-//import { ExplorerComponent } from './components/explorer/explorer.component';
-import { BikeBrandDetailsComponent } from './pages/bike-brand-details/bike-brand-details.component';
-import { BikeBrandListComponent } from './pages/bike-brand-list/bike-brand-list.component';
 import { authGuard } from './guards/auth.guard';
 
 
@@ -33,20 +30,8 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./pages/brands/brands.component')
         .then(m => m.BrandsComponent),
-    // canActivate: [authGuard]
+    canActivate: [authGuard]
   },
-  //  {
-  //   path: 'bike-brand',
-  //   component: BikeBrandListComponent,
-  //   data: { title: 'Bike Brands' }
-  // },
-  // // 🔐 Protected Bike Brand Details
-  // {
-  //   path: 'bike-brand/:bikename',
-  //   component: BikeBrandDetailsComponent,
-  //   canActivate: [authGuard],
-  //   data: { title: 'Bike Brand Details' }
-  // },
   {
     path: '**',
     redirectTo: ''
